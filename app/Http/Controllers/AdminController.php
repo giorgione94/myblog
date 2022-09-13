526c22f06cd0c4a46d36af89f69748ea71d709bf
Giorgio Biancani <giorgio.biancani@gmail.com>
2022-09-13 18:30:27 +0200

    Create AdminController, Route

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
+use App\Models\Category;
+
+class AdminController extends Controller
+{
+    public function profile() {
+        $user = Auth::user();
+        return $user;
+    }
+
+    public function post (Post $post)
+    {
+        $user = Auth::user();
+        if($user->id != $post->user_id) {
+            abort(403);
+        }
+        return $post;
+    }
+
+    public function category (Category $category) {
+        return $category;
+    }
+
+    public function newPost() {}
+
+    public function newCategory() {}
+
+    public function updateProfile() {}
+    public function updatePost() {}
+    public function updateCategory() {}
+}
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,14 @@ Route::get('/post/{post}', [App\Http\Controllers\PostController::class, 'index']
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/admin/profile', [App\Http\Controllers\AdminController::class, 'profile']);
+Route::get('/admin/post/{post}',[App\Http\Controllers\AdminController::class, 'post']);
+Route::get('/admin/category/{category}',[App\Http\Controllers\AdminController::class, 'category']);
+
+Route::post('/admin/profile', [App\Http\Controllers\AdminController::class, 'updateProfile']);
+Route::post('/admin/post/{post}',[App\Http\Controllers\AdminController::class, 'updatePost']);
+Route::post('/admin/category/{category}',[App\Http\Controllers\AdminController::class, 'updateCategory']);
+
+Route::post('/admin/post/new',[App\Http\Controllers\AdminController::class, 'newPost']);
+Route::post('/admin/category/new',[App\Http\Controllers\AdminController::class, 'newCategory']);
